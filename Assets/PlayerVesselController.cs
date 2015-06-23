f9b32b28cb9852f217299b4cc9386276efbf11c5
izbay <jacobmartinek@knights.ucf.edu>
2015-06-23 02:20:30 -0400

    Rudimentary Controls
    They're really bad, but they'll do for testing.

---
+++ Assets/PlayerVesselController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerVesselController : MonoBehaviour {
+	public float speed = 30.0f;
+	public float throttle = 10f;
+	CharacterController controller;
+
+	void Start () {
+		controller = transform.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftShift)){
+			throttle += 0.5f;
+		}
+
+		if(Input.GetKey(KeyCode.LeftControl)){
+			throttle -= 0.5f;
+		}
+
+		if(throttle < 0f) throttle = 0f;
+		if(throttle > speed) throttle = speed;
+
+		Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0,
+		                        Input.GetAxis("Vertical"));
+		moveDirection = transform.TransformDirection(moveDirection);
+		moveDirection *= throttle;
+
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+}
