9d668b21ddd97e11ebde533d712aab5f7c6eef3e
izbay <jacobmartinek@knights.ucf.edu>
2015-06-23 18:38:29 -0400

    Refined controls
    They still need some work.

--- Assets/PlayerVesselController.cs
+++ Assets/PlayerVesselController.cs
@@ -3,31 +3,52 @@ using System.Collections;
 
 public class PlayerVesselController : MonoBehaviour {
 	public float speed = 30.0f;
-	public float throttle = 10f;
+	public float throttle = 0f;
+	public float RotationSpeed = 20f;
+	public float throttleTick = 1f;
+
 	CharacterController controller;
+	Rigidbody rb;
 
 	void Start () {
 		controller = transform.GetComponent<CharacterController>();
+		rb = transform.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if(Input.GetKey(KeyCode.LeftShift)){
-			throttle += 0.5f;
-		}
+	void Update() {
 
-		if(Input.GetKey(KeyCode.LeftControl)){
-			throttle -= 0.5f;
-		}
+		Control();
+		Move();
+	}
 
+	void Control(){
+		Rotate();
+		Throttle();
+	}
+
+	void Throttle(){
+		if(Input.GetKey(KeyCode.LeftShift)){throttle += throttleTick;}
+		if(Input.GetKey(KeyCode.LeftControl)){throttle -= throttleTick;}
 		if(throttle < 0f) throttle = 0f;
 		if(throttle > speed) throttle = speed;
+	}
 
-		Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0,
-		                        Input.GetAxis("Vertical"));
-		moveDirection = transform.TransformDirection(moveDirection);
-		moveDirection *= throttle;
+	void Rotate(){
+		Quaternion AddRot = Quaternion.identity;
+		float roll = 0;
+		float pitch = 0;
+		float yaw = 0;
+		
+		roll = Input.GetAxis("Roll") * (Time.deltaTime * RotationSpeed) * 2f;
+		pitch = Input.GetAxis("Vertical") * (Time.deltaTime * RotationSpeed);
+		yaw = Input.GetAxis("Horizontal") * (Time.deltaTime * RotationSpeed) * 0.25f;
 
-		controller.Move(moveDirection * Time.deltaTime);
+		AddRot.eulerAngles = new Vector3(pitch, yaw, -roll);
+		rb.rotation *= AddRot;
+	}
+
+	void Move(){
+		transform.Translate(Vector3.forward * throttle * Time.deltaTime);
 	}
 }
